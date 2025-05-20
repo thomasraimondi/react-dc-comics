ebc8fd229f1f417b2ceac9ff31b13f0146def745
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-20 14:23:37 +0200

    add component Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <h1>Io sono il footer</h1>;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Header from "../components/header";
+import Header from "../components/Header";
 import Main from "../components/main";
+import Footer from "../components/Footer";
 
 export default function App() {
   return (
     <>
+      <h1>My React App</h1>;
       <Header />
       <Main />
-      <h1>My React App</h1>;
+      <Footer />
     </>
   );
 }
