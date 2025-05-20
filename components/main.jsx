8969ffbfa1b1a8b1d59d5c52f5322006b6046996
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-20 14:44:39 +0200

    add font family Open Sans

--- components/main.jsx
+++ components/main.jsx
@@ -1,7 +1,7 @@
 export default function Main() {
   return (
     <div id="main" className="container">
-      <h1>contenuto</h1>
+      <h1>--&gt; Content Goes Here &lt;--</h1>
     </div>
   );
 }
