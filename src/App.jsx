d8e747a49e5b5c853f8d532dbd1e61a865562769
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-20 14:11:44 +0200

    add component Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import Main from "../components/main";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Main />
       <h1>My React App</h1>;
     </>
   );
