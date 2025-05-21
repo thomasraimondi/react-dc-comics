f827a4f9f5f1f8a33911eef592a98889251514ec
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-21 16:07:08 +0200

    move widget components out of footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
 import Messages from "./components/layout/Messages";
+import Widget from "./components/layout/widget";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Messages />
       <Main />
+      <Widget />
       <Footer />
     </>
   );
--- src/components/layout/Widget.jsx
+++ src/components/layout/Widget.jsx
@@ -1,42 +1,48 @@
 export default function Widget() {
   return (
-    <div className="footer-widget">
-      <div className="footer-widget-content container">
-        <div className="col-20">
-          <img
-            src="../src/assets/img/buy-comics-digital-comics.png"
-            alt=""
-            srcset=""
-          />
-          <span>digital comics</span>
-        </div>
-        <div className="col-20">
-          <img
-            src="../src/assets/img/buy-comics-merchandise.png"
-            alt=""
-            srcset=""
-          />
-          <span>dc merchandise</span>
-        </div>
-        <div className="col-20">
-          <img
-            src="../src/assets/img/buy-comics-subscriptions.png"
-            alt=""
-            srcset=""
-          />
-          <span>subscription</span>
-        </div>
-        <div className="col-20">
-          <img
-            src="../src/assets/img/buy-comics-shop-locator.png"
-            alt=""
-            srcset=""
-          />
-          <span>comic shop locator</span>
-        </div>
-        <div className="col-20">
-          <img src="../src/assets/img/buy-dc-power-visa.svg" alt="" srcset="" />
-          <span>dc power visa</span>
+    <div className="widget">
+      <div className="widget-content container">
+        <div className="row">
+          <div className="col-1-5">
+            <img
+              src="../src/assets/img/buy-comics-digital-comics.png"
+              alt=""
+              srcset=""
+            />
+            <span>digital comics</span>
+          </div>
+          <div className="col-1-5">
+            <img
+              src="../src/assets/img/buy-comics-merchandise.png"
+              alt=""
+              srcset=""
+            />
+            <span>dc merchandise</span>
+          </div>
+          <div className="col-1-5">
+            <img
+              src="../src/assets/img/buy-comics-subscriptions.png"
+              alt=""
+              srcset=""
+            />
+            <span>subscription</span>
+          </div>
+          <div className="col-1-5">
+            <img
+              src="../src/assets/img/buy-comics-shop-locator.png"
+              alt=""
+              srcset=""
+            />
+            <span>comic shop locator</span>
+          </div>
+          <div className="col-1-5">
+            <img
+              src="../src/assets/img/buy-dc-power-visa.svg"
+              alt=""
+              srcset=""
+            />
+            <span>dc power visa</span>
+          </div>
         </div>
       </div>
     </div>
