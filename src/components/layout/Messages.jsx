d257917ed31f914b73ca928fdc37aa870afdc714
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-21 14:55:49 +0200

    fix container in messages components

--- src/components/layout/Messages.jsx
+++ src/components/layout/Messages.jsx
@@ -2,13 +2,13 @@ import Alert from "../ui/Alert";
 
 export default function Messages() {
   return (
-    <div className="container">
-      <Alert>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        dolorem natus, at mollitia dolorum dolores quod pariatur, maiores quis
-        incidunt quas delectus provident earum eligendi accusamus voluptatum
-        vitae sint enim.
-      </Alert>
+    <div id="messages">
+      <div className="container">
+        <Alert type="success">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+          amet.
+        </Alert>
+      </div>
     </div>
   );
 }
--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -1,9 +1,5 @@
 export default function Alert({ type, children }) {
   if (!children) return <></>;
 
-  return (
-    <div className="container">
-      <div className={`alert alert-${type || "danger"}`}>{children}</div>
-    </div>
-  );
+  return <div className={`alert alert-${type || "danger"}`}>{children}</div>;
 }
