c23b758aa76a9a9a8c737b57849088a4cb06b71a
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-22 14:54:08 +0200

    add parameter for hide Alert

--- src/components/layout/Messages.jsx
+++ src/components/layout/Messages.jsx
@@ -1,13 +1,17 @@
 import Alert from "../ui/Alert";
 
 export default function Messages() {
+  const hasError = false;
+
   return (
     <div id="messages">
       <div className="container">
-        <Alert type="success">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          amet.
-        </Alert>
+        {hasError && (
+          <Alert type="danger">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, amet.
+          </Alert>
+        )}
       </div>
     </div>
   );
