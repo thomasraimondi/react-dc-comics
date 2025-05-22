b5d9b41818e72df0001b63a2252b84e4261a295c
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-22 16:29:34 +0200

    add new component in main and add style

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,13 +1,24 @@
 import "../../assets/css/CssComponents/main.css";
+import ComicsCard from "../comics/ComicsCard";
+import comics from "../../assets/data/comics.js";
 
 export default function Main() {
   return (
     <div id="main">
       <div className="main-content">
         <div className="jumbotron"></div>
-        <div className="container">
-          <button className="btn">ciao</button>
-          <div className="row"></div>
+        <div className="series container">
+          <div className="section-title">current series</div>
+        </div>
+        <div className="comics-gallery container">
+          <div className="row">
+            {comics.map((comic) => (
+              <ComicsCard comic={comic} key={comic.id} />
+            ))}
+          </div>
+        </div>
+        <div className="load-more">
+          <button className="btn">Load more</button>
         </div>
       </div>
     </div>
